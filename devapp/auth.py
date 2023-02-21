196ab616d21cf1388a7cda7970cbd2921d4c80bb
Raturcu <razvan.andrei.turcu97@gmail.com>
2023-02-21 18:00:18 +0200

    Login Form working!

--- devapp/auth.py
+++ devapp/auth.py
@@ -3,23 +3,35 @@ from flask import render_template,request,redirect,url_for
 from flask import flash
 from .models import User
 from werkzeug.security import check_password_hash,generate_password_hash
-from . import db
+from devapp import db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/',methods=['GET','POST'])
-def home():
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        username=request.form.get('username')
+        password=request.form.get('password')
+        
+        user=User.query.filter_by(username=username).first()
+        
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Welcome,'+" "+username+'!' ,category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorect password',category="error")
+        else:
+            flash("Username does not exist",category='error')
+    
+    
+    if request.method == 'POST' and 'register_form' in request.form:
+        return redirect(url_for('auth.register'))         
+    
     return render_template('login.html')
-
-def register_login():
-    if request.method()=='GET': 
-        
-        return render_template('register.html')
         
     
     
-
-
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
@@ -29,7 +41,10 @@ def register():
         password1=request.form.get('password1')
         password2=request.form.get('password2')
         
-        if len(str(username)) < 3:
+        user=User.query.filter_by(username=username).first()
+        if user:
+            flash(username +"already exist",category='error')
+        elif len(str(username)) < 3:
             flash("Username must contains at least 4 characters!",category='error')
         elif len(str(email)) < 5:
             flash("Email adress must contains at least 5 characters!",category='error')
